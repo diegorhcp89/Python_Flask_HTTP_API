1d435980e0176dc96af1e0bac73ac74e7dc86ff6
diegorhcp89 <diegoferreiradoamaral@gmail.com>
2025-01-27 19:09:41 -0300

    Desenvolvimento do CRUD com Flask Create

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.taks import Task
 
 app = Flask(__name__)
 
-@app.route("/")
-def hell_word():
-    return "Hello world!"
+tasks = []
+taks_id_control = 1
 
-@app.route("/about")
-def about():
-    return "Página sobre"
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    global taks_id_control
+    data = request.get_json()
+    new_task = Task(id=taks_id_control, title=data['title'], description=data.get("description", ""))
+    taks_id_control += 1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({"message": "Nova tarefa criada com sucesso"})
 
 if __name__ == "__main__":
     app.run(debug=True)
