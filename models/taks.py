d260666d524f29525aa0f71cdeb7026f8a251e86
diegorhcp89 <diegoferreiradoamaral@gmail.com>
2025-01-28 20:37:28 -0300

    corrigindo código de to_dict

--- models/taks.py
+++ models/taks.py
@@ -10,5 +10,5 @@ class Task:
             "id": self.id,
             "title": self.title,
             "description": self.description,
-            "completed": self.description
+            "completed": self.completed
         }
